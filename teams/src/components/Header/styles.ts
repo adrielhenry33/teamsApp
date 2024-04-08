280541cd1bb14c37f89d38a05726f0292f7943c1
adrielhenry33 <adrielhenrydb@gmail.com>
2024-04-08 10:20:15 -0300

    criando o icone de seta e tornando ele uma regiao clicavel

--- teams/src/components/Header/styles.ts
+++ teams/src/components/Header/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components/native';
+import { Car, CaretLeft } from "phosphor-react-native"; //icone 
+
 
 export const Container = styled.View`
   width: 100%;
@@ -13,3 +15,12 @@ export const Logo = styled.Image   `
     height: 55px ;
     
 `;
+
+export const BackButton = styled.TouchableOpacity `
+    flex : 1;
+`;
+
+export const BackIcon = styled(CaretLeft).attrs(({ theme}) => ({
+    size: 36,
+    color: theme.COLORS.WHITE
+}))``;
