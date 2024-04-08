336bd8f91c3eb1e9e855e647e8ef04183a2824f4
adrielhenry33 <adrielhenrydb@gmail.com>
2024-04-08 09:57:06 -0300

    criando o componente header e o arquivo de tipagem para as logos

--- teams/src/screens/Groups/index.tsx
+++ teams/src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
 
 import { Container } from './styles';
+import { Header } from '@components/Header';
 
 export default function Groups() {
   return (
     <Container>
-      
+      <Header/>
     </Container>
   );
 }
