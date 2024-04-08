d5fd2b63cba050b63a3459c43e9668b4748ffc32
adrielhenry33 <adrielhenrydb@gmail.com>
2024-04-07 21:33:59 -0300

    importando as fontes

--- teams/App.tsx
+++ teams/App.tsx
@@ -1,8 +1,9 @@
 import Groups from '@screens/Groups';
 
 import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import theme from './src/themes'
+import theme from './src/theme';
 
 export default function App() {
   return (
--- teams/src/screens/Groups/index.tsx
+++ teams/src/screens/Groups/index.tsx
@@ -1,13 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-import { Container , Title } from './styles';
+import { Container  } from './styles';
 
 export default function Groups() {
   return (
     <Container>
-      <Title>
-        adfadfadfadfadfaf
-      </Title>
+      
     </Container>
   );
 }
