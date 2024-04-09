d5e681aed79921b432625d974a09370e9eed2e68
adrielhenry33 <adrielhenrydb@gmail.com>
2024-04-09 11:55:01 -0300

    criando titulo e subtitulo da aplicacao de forma dinaminca passando, foi criado um tipo para ambos e declarado o titulo e o subtitulo no groups

--- teams/src/screens/Groups/index.tsx
+++ teams/src/screens/Groups/index.tsx
@@ -1,11 +1,17 @@
 
 import { Container } from './styles';
 import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight/indext';
+
 
 export default function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight
+        title = "Turmas"
+        subtitle="jogue com sua turma"
+        />
     </Container>
   );
 }
