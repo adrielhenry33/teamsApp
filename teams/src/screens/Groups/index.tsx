573114723597bf9774ec129a3de127f0df7b7149
adrielhenry33 <adrielhenrydb@gmail.com>
2024-04-11 16:42:45 -0300

    criando a listagem de turmas

--- teams/src/screens/Groups/index.tsx
+++ teams/src/screens/Groups/index.tsx
@@ -3,16 +3,34 @@ import { Container } from './styles';
 import { Highlight } from '@components/Highlight/indext';
 import { GroupCard } from '@components/GroupCard';
 
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 
 export default function Groups() {
+  
+  const [groups, setGroups] = useState<string[]>(['Rocktet', 'Cruzeiro']);
+  
   return (
     <Container>
       <Header />
+      
       <Highlight
         title = "Turmas"
         subtitle="Jogue com sua turma"
       />
-      <GroupCard title="Galera" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item} //extrair uma valor unico para cada item da lista
+        renderItem={({ item }) => (
+          <GroupCard
+          title={item}
+          />
+        )}
+      />
+
+      
     </Container>
   );
 }
