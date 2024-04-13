2e8f990bc5398d32e0c8996d531188251237ce1f
adrielhenry33 <adrielhenrydb@gmail.com>
2024-04-13 16:01:06 -0300

    criando o componente de formulario dinamincamente

--- teams/src/screens/NewGroups/index.tsx
+++ teams/src/screens/NewGroups/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon } from './styles';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight/indext';
 import { Button } from '@components/Button';
+import { Input } from '@components/InputText/indext';
 
 
 export function NewGroup(){
@@ -16,8 +17,11 @@ export function NewGroup(){
                     title="Nova Turma"
                     subtitle= "Crie uma nova turma para adicionar as pessoas"
                 />
+
+                <Input/>
                 <Button
                     title="Criar"
+                    style ={ {marginTop: 20} }
                 />
             </Content>
 
