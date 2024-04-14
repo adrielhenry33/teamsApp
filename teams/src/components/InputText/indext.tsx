4bee75bad4e085a6da74dc0677433919730eaec9
adrielhenry33 <adrielhenrydb@gmail.com>
2024-04-14 12:12:52 -0300

    placeholder estilizado, usando os temas pre definidos

--- teams/src/components/InputText/indext.tsx
+++ teams/src/components/InputText/indext.tsx
@@ -2,9 +2,20 @@ import { Container } from './styles';
 
 import { TextInputProps } from 'react-native';
 
-export function Input({...rest}: TextInputProps){
+import { useTheme } from 'styled-components/native';
+
+
+export function Input( { ...rest }: TextInputProps){
+
+    const { COLORS } = useTheme();
+
+
     return(
         <Container
+            placeholderTextColor={COLORS.GRAY_300} // AQUI ESTAMOS ESTILIZANOD O PLACEHOLDER ATRAVES
+                                                   // DOS TEMAS PRE DEFINIDOS UTILIZANDO UM ESTADO
+                                                   // COMO QUEREMOS SO AS CORES DESESTRUTURAMOS SOMENTE
+                                                   // COLORS E NAO TODO O THEME 
             {...rest}
         />
     );
