525ce0593a4b5dd67e3a33fda214753d19032374
adrielhenry33 <adrielhenrydb@gmail.com>
2024-04-23 22:09:39 -0300

    add

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+import { Car, CaretLeft } from "phosphor-react-native"; //icone 
+
+
+export const Container = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+`;
+
+export const Logo = styled.Image   `
+    width: 46px;
+    height: 55px ;
+    
+`;
+
+export const BackButton = styled.TouchableOpacity `
+    flex : 1;
+`;
+
+export const BackIcon = styled(CaretLeft).attrs(({ theme}) => ({
+    size: 36,
+    color: theme.COLORS.WHITE
+}))``;
