da343a6787d2953cb270205fd5159802cd6ff3c1
adrielhenry33 <adrielhenrydb@gmail.com>
2024-05-01 20:37:00 -0300

    criado a classe para tratamento de erros;

--- src/storage/group/groupCreat.ts
+++ src/storage/group/groupCreat.ts
@@ -3,11 +3,20 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GROUP_COLLECTION } from "./storageConfig";
 import { groupsGetAll } from "./groupsGetAll";
 
+import { AppError } from "@utils/AppErro";
+
 export async function groupCreate (newGroup: string) {
     try{
         //armazenando na variavel todos os grupos que ja estao salvos
         const storedGroups = await groupsGetAll();
         
+        //verifica se esta incluso
+        const groupAlreadyExist = storedGroups.includes(newGroup);
+        
+        if (groupAlreadyExist) {
+            throw new AppError("Ja existe um grupo com esse nome!");
+        }
+
         //transformando em string ja que o groupsGetAll retorna um objeto
         //pegando tudo que esta na string e o novo grupo e adicionando em storage
         const storage = JSON.stringify([...storedGroups, newGroup]);
